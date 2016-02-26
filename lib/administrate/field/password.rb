7ed0743e0d1d6ecd9df519890161391b507af22d
Adrian Rangel <adrian.rangel@gmail.com>
2016-02-26 13:49:14 -0600

    genesis commit

---
+++ lib/administrate/field/password.rb
@@ -0,0 +1,26 @@
+require 'administrate/field/base'
+require 'rails'
+
+module Administrate
+  module Field
+    class Password < Administrate::Field::Base
+      VERSION = '0.0.1'
+      class Engine < ::Rails::Engine
+      end
+
+      def self.searchable?
+        false
+      end
+
+      def truncate
+        pretty_data.to_s[0...6]
+      end
+
+      private
+
+      def pretty_data
+        data.gsub(/./, "•") unless data.nil?
+      end
+    end
+  end
+end
---
+++ spec/lib/administrate/field/password_spec.rb
@@ -0,0 +1,14 @@
+require 'administrate/field/password'
+
+describe Administrate::Field::Password do
+  describe '#to_partial_path' do
+    it 'returns a partial based on the page being rendered' do
+      page = :show
+      field = Administrate::Field::Password.new(:password, '3lkajsd', page)
+
+      path = field.to_partial_path
+
+      expect(path).to eq("/field/password/#{page}")
+    end
+  end
+end
